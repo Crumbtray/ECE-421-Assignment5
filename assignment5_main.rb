0613ea3d663493a907f99c7ffd3ae0d1de97d082
Crumbtray <clint.isaac.wong@gmail.com>
2013-12-06 23:53:29 -0700

    More instructions for TA

--- assignment5_main.rb
+++ assignment5_main.rb
@@ -6,6 +6,7 @@
 # Clinton Wong
 
 puts "Attention TA: "
+puts "To run the server, please \"ruby connect_four_server.rb\""
 puts "You will have to adjust the IP address of our client to match that of the server."
 puts "To do so, please change the code in connect_four_client.rb"
 require './connect_four_ui'
