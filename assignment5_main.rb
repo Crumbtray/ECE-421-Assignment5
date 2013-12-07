6a1d422e799580c29beb04ee4ca5fc9b6d897065
Crumbtray <clint.isaac.wong@gmail.com>
2013-12-06 23:43:56 -0700

    Added warnings for TA

--- assignment5_main.rb
+++ assignment5_main.rb
@@ -5,4 +5,7 @@
 # Tyler Schneider
 # Clinton Wong
 
+puts "Attention TA: "
+puts "You will have to adjust the IP address of our client to match that of the server."
+puts "To do so, please change the code in connect_four_client.rb"
 require './connect_four_ui'
