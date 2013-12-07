182d0a2608318c7a3c0deb8a55276aca7a7d54ad
cbeckett <cbeckett@ualberta.ca>
2013-12-06 18:01:08 -0700

    added methods for save and load of games

--- connect_four_database.rb
+++ connect_four_database.rb
@@ -7,25 +7,53 @@ class ConnectFourDatabase
 		####@db = Mysql.new("mysqlsrv.ece.ualberta.ca", "group1" , "GF2OHhMfWY7s", "C410test", 13010)
 	end
 
-	def saveGame(game)
-		# Pre conditions
-		# Make sure that the game hasn't ended.
-		begin
-			raise ArgumentError, "Game has ended." unless game.gameBoard.endGame == false
-		end
-		# End Pre Conditions
-
+	def reset
+		@db.query("DROP TABLE IF EXISTS savedGames")
+		@db.query("CREATE TABLE savedGames	\
+              (	\
+                player1     CHAR(40) NOT NULL,	\
+                player2     CHAR(40) NOT NULL,	\
+				gameType     CHAR(40) NOT NULL,	\
+				PRIMARY KEY ( player1 )
+              )
+            ")
+	end
+	
+	def saveGame(player1_, player2_, gameType_)
+		#Insert entry into table (overwrite if past saved game exists)
+		@db.query("INSERT INTO savedGames (player1, player2, gameType)	\
+						VALUES ('#{player1_}', '#{player2_}', '#{gameType_}')	\
+					ON DUPLICATE KEY UPDATE (player2, gameType)	\
+						VALUES ('#{player2_}', '#{gameType_}')")
+						
+		# Post Conditions 
+		#Ensure that there is one entry for our newly inserted saved game
+		res = @db.query("SELECT * FROM savedGames(player1) VALUES('#{player1_}')")
+		assert(res.count == 1)
+		#End Post Conditions						
 	end
 
-	def loadGame(gameId)
-		res = @db.query('select * from animal')
-		#Pre conditions
-			# Make sure that it exists in the database.
-			assert(res.num_rows == 1)
-		# End Pre conditions
-
+	def loadGame(player_)
 		# Post Conditions 
-			# Database does not have it any more.
+		# Database has no more than one entry
+		res = @db.query("SELECT * FROM savedGames(player) VALUES('#{player_}')")
+		assert(res.count <= 1)
+		# End Post Conditions
+		
+		player2 = nil
+		gameType = nil
+		res = @db.query("SELECT * FROM savedGames(player) VALUES('#{player_}')")
+		res.each_hash do |h| 
+			player2 = h['player2']
+			gameType = h['gameType']
+		end
+		
+		@db.query("DELETE FROM savedGames(player) VALUES('#{player_}')")
+		
+		# Post Conditions 
+		# Database does not have it any more.
+		res = @db.query("SELECT * FROM savedGames(player) VALUES('#{player_}')")
+		assert(res.count == 0)
 		# End Post Conditions
 	end
 
